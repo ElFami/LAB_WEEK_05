7be9850515750f9892eddc4f35911a2ce641f4e3
ElFami <eldosan0805@gmail.com>
2025-09-28 18:15:57 +0700

    “Commit No. 3 - Load and Display Cat Image using Glide

--- app/src/main/java/com/example/lab_week_05/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_05/MainActivity.kt
@@ -14,6 +14,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import com.example.lab_week_05.api.CatApiService
 import com.example.lab_week_05.model.ImageData
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,13 +25,23 @@ class MainActivity : AppCompatActivity() {
             .build()
     }
 
-    private val apiResponseView: TextView by lazy {
-        findViewById<TextView>(R.id.api_response)
-    }
+//    private val apiResponseView: TextView by lazy {
+//        findViewById<TextView>(R.id.api_response)
+//    }
 
-    private val catApiService: CatApiService by lazy {
+    private val catApiService by lazy{
         retrofit.create(CatApiService::class.java)
     }
+    private val apiResponseView: TextView by lazy{
+        findViewById(R.id.api_response)
+    }
+    private val imageResultView: ImageView by lazy {
+        findViewById(R.id.image_result)
+    }
+    private val imageLoader: ImageLoader by lazy {
+        GlideLoader(this)
+    }
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,11 +65,16 @@ class MainActivity : AppCompatActivity() {
             override fun onFailure(call: Call<List<ImageData>>, t: Throwable) {
                 Log.e(MAIN_ACTIVITY, "Failed to get response", t)
             }
-            override fun onResponse(call: Call<List<ImageData>>,
-                                    response: Response<List<ImageData>>) {
+            override fun onResponse(call: Call<List<ImageData>>, response:
+            Response<List<ImageData>>) {
                 if(response.isSuccessful){
                     val image = response.body()
-                    val firstImage = image?.firstOrNull()?.imageUrl ?: "No URL"
+                    val firstImage = image?.firstOrNull()?.imageUrl.orEmpty()
+                    if (firstImage.isNotBlank()) {
+                        imageLoader.loadImage(firstImage, imageResultView)
+                    } else {
+                        Log.d(MAIN_ACTIVITY, "Missing image URL")
+                    }
                     apiResponseView.text = getString(R.string.image_placeholder,
                         firstImage)
                 }
@@ -70,6 +86,7 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
+
 
 
     companion object {
